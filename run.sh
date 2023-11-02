862be1c1bfc972178600e8975b78f47741020d48
Leo Chen <leo.chen@vrviu.com>
2023-11-02 11:50:11 +0800

    add libkenlm.so

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 # 拷贝escape_pod依赖的第三方私有共享库
 chmod +x ./lib/libstt.so
 cp ./lib/libstt.so /usr/bin/
+chmod +x ./lib/libkenlm.so
+cp ./lib/libkenlm.so /usr/bin/
 # 拷贝escape-pod配置
 cp ./escape-pod.conf /etc/escape-pod.conf
 cp ./escape_pod.service /lib/systemd/system/escape_pod.service
